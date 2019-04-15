a3633c6a87668e13ca54ca2df8283d56b8436afe
Esherm27 <49043657+Esherm27@users.noreply.github.com>
2019-04-15 10:29:21 -0400

    commented and slightly working

--- Algorithms/BioProject2.py
+++ Algorithms/BioProject2.py
@@ -2,7 +2,7 @@ import re
 import collections
 import csv
 import sys 
-import GUI as data
+#import GUI as data
 from string import *
 
 
@@ -12,12 +12,126 @@ list1 = "ATGTAGTGTATAAAGTACATGCA"
 #s2 = input('Enter a file name: ')
 list2 = "ATGTAGTACATGCA"
 
-    
+list1 = "ACGTCAGGG"
+list2 = "ACGTCAGGC"
 
-def brutForce(s1, s2):
-    min = 0
-    match = 4
+
+def DivideConquer(s1, s2):# devideds up strings and compares them
+    max = 0
+    tmp = 0
+    match = 1
     gap = -2
+    mismatch = -1
+    score = 0
+    temp1 = s1
+    temp2 = s2
+    best1 = ""
+    best2 = ""
+
+    if(s1 == s2):
+        print(s1)
+        print(s2)
+        print("score" + len(s1))
+
+    #elif():
+
+
+
+
+DivideConquer(list1, list2)
+
+def Greedy(s1, s2):#finds the fastest soluion 
+    max = 0
+    tmp = 0
+    match = 1 # scoring for difference in strings
+    gap = -2
+    mismatch = -1
+    score = 0
+    temp1 = s1
+    temp2 = s2
+    best1 = ""
+    best2 = ""
+
+    if(s1 == s2):#if  strings are exactly the same 
+        print(s1)
+        print(s2)
+        print("score" + len(s1))
+
+    elif(len(s1) == len(s2)):#if theyre the same length loops throught 
+        for i in range(len(s1)):
+            if(s1[i]==s2[i]):# if characters match add 1 to match 
+                score += match
+            else:# if they dont match check what else is there
+                tmp = score
+                for j in range ((len(s1)-i)):
+
+                    if(s1[i+j]==s2[i+j]):#misMatches 
+                        score += mismatch*j
+
+                        i += j
+                        break
+
+                if(tmp == score):#end cases 
+                    score = mismatch*(len(s1)-i)
+                print(score)
+                if(max < score):# keeps track of highest score for comapred strings 
+                      max = score
+                      best1 = s1
+                      best2 = s2
+                score = tmp
+                s1 = temp1
+                s2 = temp2
+                
+                for j in range ((len(s1)-i)):
+                    if(s1[i]==s2[i+j]):# checks for gaps
+                        score += gap*j
+                        for k in range (j):
+                            s1 = s1[:i+k] + '-' + s1[i+k:]
+                        i += j
+                        break
+
+                print(score)
+                if(max < score):# keeps track of highest score for comapred strings 
+                      max = score
+                      best1 = s1
+                      best2 = s2
+                score = tmp
+                s1 = temp1
+                s2 = temp2
+
+
+                for j in range ((len(s1)-i)-1):
+                    if(s1[i+j]==s2[i]):
+                        score += gap*j
+                        for k in range (j):
+                            s2 = s2[:i+k] + '-' + s2[i+k:]
+                        i += j
+                        break
+
+                print(score)
+                if(max < score):# keeps track of highest score for comapred strings 
+                     max = score
+                     best1 = s1
+                     best2 = s2
+                score = tmp
+                s1 = temp1
+                s2 = temp2
+
+    print("Greedy")
+    print(best1)
+    print(best2)
+    print("Score: " , max)              
+                
+
+Greedy(list1,list2)
+
+
+
+def brutForce(s1, s2):#checks all the passible options to find the best score 
+    max = 0
+    tmp = 0
+    match = 1
+    gap = -2 # scoring for strings 
     mismatch = -1
     score = 0
     temp1 = s1
@@ -26,49 +140,53 @@ def brutForce(s1, s2):
     best2 = ""
     
 
-    if(s1 == s2):
+    if(s1 == s2):#inf thy are the same 
         print(s1)
         print(s2)
-        print("score: 0 ")
+        print("score: " + len(s1))
 
-    elif(len(s1) == len(s2)):
+    elif(len(s1) == len(s2)):#if they are the same length
 
         for i in range(len(s1)-1):
             if(s1[i]==s2[i]):
                 score += match
             else:
+                tmp = score
                 for j in range ((len(s1)-i)-1):
 
-                    if(s1[i]==s2[i+j]):
+                    if(s1[i]==s2[i+j]):# checks for gaps
                         score += gap*j
                         for k in range (j):
                             s1 = s1[:i+k] + '-' + s1[i+k:]
                         i += j
                         break
 
-                    elif(s1[i+j]==s2[i]):
+                    elif(s1[i+j]==s2[i]):# checks for gaps
                         score += gap*j
                         for k in range (j):
                             s2 = s2[:i+k] + '-' + s2[i+k:]
                         i += j
                         break
 
-                    elif(s1[i+j]==s2[i+j]):
+                    elif(s1[i+j]==s2[i+j]):# checks for mismatch 
                         score += mismatch*j
                         i += j
                         break
+                if(tmp == score):
+                    score = mismatch*(len(s1)-i)
         
-        min = score
-        score = 0 
+        max = score
+        score = 0 # keeps track of highest score for comapred strings 
         best1 = s1
         best2 = s2
         s1 = temp1
         s2 = temp2
 
-        for i in range(len(s1)-1):
+        for i in range(len(s1)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
+                tmp = score
                 for j in range ((len(s1)-i)-1):
 
                     if(s1[i+j]==s2[i]):
@@ -89,19 +207,22 @@ def brutForce(s1, s2):
                         score += mismatch*j
                         i += j
                         break
-        
-        if(score < min):
-            min = score
+                if(tmp == score):
+                    score = mismatch*(len(s1)-i)
+
+        if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
         score = 0
         s1 = temp1
         s2 = temp2
 
-        for i in range(len(s1)-1):
+        for i in range(len(s1)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
+                tmp = score
                 for j in range ((len(s1)-i)-1):
 
                     if(s1[i+j]==s2[i+j]):
@@ -122,19 +243,22 @@ def brutForce(s1, s2):
                             s1 = s1[:i+k] + '-' + s1[i+k:]
                         i += j
                         break
+                if(tmp == score):
+                    score = mismatch*(len(s1)-i)
         
-        if(score < min):
-            min = score
+        if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
         score = 0
         s1 = temp1
         s2 = temp2
                    
-        for i in range(len(s1)-1):
+        for i in range(len(s1)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
+                tmp = score
                 for j in range ((len(s1)-i)-1):
 
                     if(s1[i+j]==s2[i+j]):
@@ -155,9 +279,11 @@ def brutForce(s1, s2):
                             s2 =  s2[:i+k] + '-' + s2[i+k:]
                         i += j
                         break
+                if(tmp == score):
+                    score = mismatch*(len(s1)-i)
         
-        if(score < min):
-            min = score
+        if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
         score = 0
@@ -165,7 +291,7 @@ def brutForce(s1, s2):
         s2 = temp2
 
 
-    elif(len(s1) > len(s2)):
+    elif(len(s1) > len(s2)):# runs similar loops to check all possible combinations 
 
         for i in range(len(s2)-1):
             if(s1[i]==s2[i]):
@@ -193,14 +319,15 @@ def brutForce(s1, s2):
                         break
 
         score += (len(s1)-len(s2))*gap
-        min = score
+        if(score > max):# keeps track of highest score for comapred strings 
+            max = score
+            best1 = s1
+            best2 = s2
         score = 0 
-        best1 = s1
-        best2 = s2
         s1 = temp1
         s2 = temp2
 
-        for i in range(len(s2)-1):
+        for i in range(len(s2)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
@@ -226,15 +353,15 @@ def brutForce(s1, s2):
                         break
 
         score += (len(s1)-len(s2))*gap
-        if(score < min):
-            min = score
+        if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
         score = 0
         s1 = temp1
         s2 = temp2
 
-        for i in range(len(s2)-1):
+        for i in range(len(s2)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
@@ -260,15 +387,15 @@ def brutForce(s1, s2):
                         break
 
         score += (len(s1)-len(s2))*gap
-        if(score < min):
-            min = score
+        if(score > max ):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
         score = 0
         s1 = temp1
         s2 = temp2
                    
-        for i in range(len(s2)-1):
+        for i in range(len(s2)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
@@ -294,8 +421,8 @@ def brutForce(s1, s2):
                         break
 
         score += (len(s1)-len(s2))*gap
-        if(score < min):
-            min = score
+        if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
         score = 0
@@ -304,9 +431,9 @@ def brutForce(s1, s2):
 
                     
 
-    else:
+    else: #if the strings are not the same length
 
-          for i in range(len(s1)-1):
+          for i in range(len(s1)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
@@ -331,14 +458,15 @@ def brutForce(s1, s2):
                         i += j
                         break
           score += (len(s2)-len(s1))*gap
-          min = score
-          score = 0 
-          best1 = s1
-          best2 = s2
+          if(score > max):# keeps track of highest score for comapred strings 
+            max = score
+            best1 = s1
+            best2 = s2
+          score = 0
           s1 = temp1
           s2 = temp2
 
-          for i in range(len(s1)-1):
+          for i in range(len(s1)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
@@ -363,15 +491,15 @@ def brutForce(s1, s2):
                         i += j
                         break
           score += (len(s2)-len(s1))*gap
-          if(score < min):
-            min = score
+          if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
           score = 0
           s1 = temp1
           s2 = temp2
 
-          for i in range(len(s1)-1):
+          for i in range(len(s1)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
@@ -396,15 +524,15 @@ def brutForce(s1, s2):
                         i += j
                         break
           score += (len(s2)-len(s1))*gap
-          if(score < min):
-            min = score
+          if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
           score = 0
           s1 = temp1
           s2 = temp2
                    
-          for i in range(len(s1)-1):
+          for i in range(len(s1)-1):# runs similar loops to check all possible combinations 
             if(s1[i]==s2[i]):
                 score += match
             else:
@@ -429,8 +557,8 @@ def brutForce(s1, s2):
                         i += j
                         break
           score += (len(s1)-len(s2))*gap
-          if(score < min):
-            min = score
+          if(score > max):# keeps track of highest score for comapred strings 
+            max = score
             best1 = s1
             best2 = s2
           score = 0
@@ -438,10 +566,10 @@ def brutForce(s1, s2):
           s2 = temp2
 
                    
-
+    print("brutforce")
     print(best1)
     print(best2)
-    print("Score: " , min)
+    print("Score: " , max)
 
 
 brutForce(list1,list2)
